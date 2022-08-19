97336b6961bbc931e4b38e6fe57542ae50ad96ea
Giovanna Zaccarelli <giovannazaccarelli@alunos.utfpr.edu.br>
2022-08-19 15:28:31 -0300

    Adding Property Binding while calling logo.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  logoMenuImage: string = '../../assets/resources/images/logo.png';
+
   constructor() { }
 
   ngOnInit(): void {
