e790e28c8b188345189ebf2e479c9cd5ea2cf443
Giovanna Zaccarelli <giovannazaccarelli@alunos.utfpr.edu.br>
2022-08-27 16:58:20 -0300

    Creating and styling Initial Page and Main Page; Adjusting other items and creating components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EnterAppComponent } from './enter-app/enter-app.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
 
-const routes: Routes = [{path: '', component: EnterAppComponent}];
+const routes: Routes = [
+  {path: '', component: InitialPageComponent},
+  {path: 'login', component: EnterAppComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { EnterAppComponent } from './enter-app/enter-app.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FooterComponent,
-    EnterAppComponent
+    EnterAppComponent,
+    InitialPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
