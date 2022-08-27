85f3de32bf522de249cc6bc08a890293ab28c090
Giovanna Zaccarelli <giovannazaccarelli@alunos.utfpr.edu.br>
2022-08-27 14:24:37 -0300

    Creating and styling Enter App page.

---
+++ src/app/enter-app/enter-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-app',
+  templateUrl: './enter-app.component.html',
+  styleUrls: ['./enter-app.component.css']
+})
+export class EnterAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
