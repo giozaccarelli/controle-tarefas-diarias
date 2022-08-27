fe53028b3cef79332c4ee4f7b4e6d4918e3a7cfc
Giovanna Zaccarelli <giovannazaccarelli@alunos.utfpr.edu.br>
2022-08-27 17:27:17 -0300

    Passing the username as a parameter and showing it on the main page as an example to this lesson.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EnterAppComponent } from './enter-app/enter-app.component';
 import { InitialPageComponent } from './initial-page/initial-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   {path: '', component: InitialPageComponent},
-  {path: 'login', component: EnterAppComponent}];
+  {path: 'login', component: EnterAppComponent},
+  {path: 'login/:usuario', component: MainPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  nomeUsuario: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    this.nomeUsuario = this.activatedRoute.snapshot.paramMap.get("usuario");
   }
 
 }
